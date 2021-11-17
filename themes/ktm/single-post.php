c3cabc6a7c8d8bd59b0b8f92395efd1e7d428bb9
Hemnath Mouli <hemnathmouli@gmail.com>
2021-11-17 18:42:47 +0000

    Test: Category links update

--- themes/ktm/single-post.php
+++ themes/ktm/single-post.php
@@ -25,7 +25,7 @@
                                     <path fill-rule="evenodd" clip-rule="evenodd" d="M3.9 2.6H2.6V1.94998H1.3V3.9H11.7V1.94998H10.4V2.6H9.1V1.94998H3.9V2.6ZM11.7 5.2H1.3V11.7H11.7V5.2ZM9.1 0.65H3.9V0H2.6V0.65H1.3C0.58203 0.65 0 1.23203 0 1.95V11.7C0 12.418 0.58203 13 1.3 13H11.7C12.418 13 13 12.418 13 11.7V1.95C13 1.23203 12.418 0.65 11.7 0.65H10.4V0H9.1V0.65ZM3.25 7.8V6.5H4.55V7.8H3.25ZM5.85 7.8H7.15V6.5H5.85V7.8ZM8.45 7.8V6.5H9.75V7.8H8.45ZM3.25 9.1V10.4H4.55V9.1H3.25ZM7.15 10.4H5.85V9.1H7.15V10.4Z" fill="#FFC220"/>
                                 </svg>
 
-                                <?= date( 'M nS, Y', strtotime($post->post_date) ); ?>
+                                <?= date( 'M S, Y', strtotime($post->post_date) ); ?>
                             </li>
 
                             <li>
@@ -44,7 +44,17 @@
                                 <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" clip-rule="evenodd" d="M4.08449 3.125H13.125C13.8154 3.125 14.375 3.68464 14.375 4.375V10.625C14.375 11.3154 13.8154 11.875 13.125 11.875H4.08449C3.66655 11.875 3.27626 11.6661 3.04443 11.3184L0.729969 7.84669C0.59001 7.63675 0.59001 7.36325 0.729969 7.15331L3.04443 3.68162C3.27626 3.33388 3.66655 3.125 4.08449 3.125ZM2.00113 7.5L4.08446 10.625H13.125V4.375H4.08446L2.00113 7.5ZM5 7.5C5 7.84518 4.72018 8.125 4.375 8.125C4.02982 8.125 3.75 7.84518 3.75 7.5C3.75 7.15482 4.02982 6.875 4.375 6.875C4.72018 6.875 5 7.15482 5 7.5Z" fill="#FFC220"/>
                                 </svg>
-                                General Knowledge
+                                
+
+                                <?php 
+                                    $categories =   wp_get_post_categories( $post->ID );
+                                    $cat_string  =   array();
+                                    foreach ( $categories as $category ){
+                                        $cat_string[] = "<a href='".get_term_link( $category )."' title='{$category->name}'>{$category->name}</a>";
+                                    }
+
+                                    echo implode( ", ", $cat_string );
+                                ?>
                             </li>
                         </ul>
                     </div>
