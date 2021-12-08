3210b957d79c2d9f6a2626c9e12e475505af7378
Hemnath Mouli <hemnathmouli@gmail.com>
2021-12-08 13:56:54 +0000

    Add form fields to Job Posting

--- themes/ktm/single-job_posting.php
+++ themes/ktm/single-job_posting.php
@@ -6,6 +6,24 @@
         <div class="job-posting-content">
             <?php the_content(); ?>
         </div>
+
+        <div class="job-posting-apply-now">
+            <!-- ACF Fields: Job Applicants Fields -->
+            <h3>Apply for this position: <?php the_title(); ?></h3>
+            <?php
+                acf_form(array(
+                    'post_id'		=> 'new_post',
+                    'field_groups'	=> array( 'group_61b0b670bbe5f' ),
+                    'new_post'		=> array(
+                        'post_type'		=> 'job_applicant',
+                        'post_status'	=> 'publish'
+                    ),
+                    'post_title'	=> true,
+                    'uploader'		=> 'basic',
+                    'submit_value'	=> 'Apply Now'
+                ));
+            ?>
+        </div>
     <?php endwhile; ?>
 </div>
 
