825e5734298f71e9c47f58869619209bdab3cafe
Hemnath Mouli <hemnathmouli@gmail.com>
2021-12-09 01:56:47 +0000

    Job posting header title update

--- themes/ktm/single-job_posting.php
+++ themes/ktm/single-job_posting.php
@@ -1,8 +1,10 @@
 <?php get_header(); ?>
 
+<?php ktm_header_title(); ?>
+
+
 <div class="container job-posting-container">
     <?php while ( have_posts() ) : the_post(); ?>
-        <h1 class="job-posting-title"><?php the_title(); ?></h1>
         <div class="job-posting-content">
             <?php the_content(); ?>
         </div>
